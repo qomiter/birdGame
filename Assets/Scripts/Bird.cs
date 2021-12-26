8a670d9e55b689e2cd45ceafdc5d69c0a3a157be
qomiter <qomiter@gmail.com>
2021-12-26 12:40:24 -0500

    Fixed bug

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.blue;
+    }
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
